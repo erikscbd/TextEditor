639549cbe33e32db576e58673540e68447ebd65c
erik escobedo <erikscbd@gmail.com>
2022-07-24 14:50:17 -0500

    made updates to src-sw and install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,20 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    butInstall.hidden = false;
+    butInstall.addEventListener('click', (event) => {
+        event.prompt();
+        butInstall.setAttribute('disabled', true);
+        butInstall.textContent = 'Installed';
+    });
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    textHeader.textContent = 'Installed Successfully';
+    console.log('Installed Successfully', event);
+});
